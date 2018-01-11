9204ddd2b6974609f9479de16ccc99abf0cd4661
Joel Thoms <jthoms@gmail.com>
2018-01-11 14:58:02 -0800

    tracker made more functional

--- src/recipes/tracker/index.js
+++ src/recipes/tracker/index.js
@@ -7,17 +7,21 @@ const dataDir = `${process.cwd()}/.data`
 const getFileDate = date =>
   moment(date).format('YYYY-MM-DD')
 
+const getFileName = dir =>
+  `${dir}/tracker-${getFileDate(new Date())}.csv`
+
+const formatLine = ({ exchange, trade: { trade_id, side, size, price, product_id, time } }) =>
+  [trade_id, exchange, side, size, price, product_id, time].join(',')
+
 export default async ({
   dependencies: { fs, log },
   events,
 } = {}) => {
   Observable.fromEvent(events, 'gdax.TRADE')
-    .subscribe(({ exchange, trade: { trade_id, side, size, price, product_id, time } }) => {
-      log.trace('track', JSON.stringify({ price, product_id }))
-      const file = `${dataDir}/tracker-${getFileDate(new Date())}.csv`
-      const line = [trade_id, exchange, side, size, price, product_id, time].join(',')
+    .subscribe((data) => (
+      log.trace('track', JSON.stringify({ price: data.trade.price, trade: data.trade.product_id })),
       fs.ensureDir(dataDir)
-        .then(() => fs.appendFile(file, `${line}\n`, 'utf8'))
+        .then(() => fs.appendFile(getFileName(dataDir), `${formatLine(data)}\n`, 'utf8'))
         .catch(err => log.error(err))
-    })
+    ))
 }
