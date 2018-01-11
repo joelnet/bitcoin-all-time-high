78438f8a1959d9c2fc3beda35437e53d49e171c8
Joel Thoms <jthoms@gmail.com>
2018-01-10 23:27:20 -0800

    completely unnecessary refactoring

--- src/lib/datastore.js
+++ src/lib/datastore.js
@@ -2,10 +2,15 @@ import Datastore from 'nedb'
 import promisify from 'functional-helpers/promisify'
 import when from 'ramda/src/when'
 
+const indexFieldNotEmpty = x => x
+
+const ensureIndex = db => fieldName =>
+  db.ensureIndex({ fieldName, unique: true })
+
 export default ({ filename, indexField }) => {
   const db = new Datastore({ filename, autoload: true })
 
-  when(x => x, fieldName => db.ensureIndex({ fieldName, unique: true }), indexField)
+  when(indexFieldNotEmpty, ensureIndex(db), indexField)
 
   return {
     find: promisify(db.find, db),
--- src/plugins/media/index.js
+++ src/plugins/media/index.js
@@ -1,27 +1,31 @@
 import PImage from 'pureimage'
 import getImageFromFile from './getImageFromFile'
 import imageToBase64 from './imageToBase64'
+import pipeAsync from '../../lib/pipeAsync'
 
 const registerFont = (file, name) =>
   new Promise(resolve => PImage.registerFont(file, name).load(resolve))
 
 const writeText = (img, item) => {
   const ctx = img.getContext('2d')
-  Object.assign(ctx, { fillStyle: item.color, font: `${item.size}pt 'Custom Font'` })
-  ctx.fillText(item.text, item.x, item.y)
-  return img
+  return (
+    Object.assign(ctx, { fillStyle: item.color, font: `${item.size}pt 'Custom Font'` }),
+    ctx.fillText(item.text, item.x, item.y),
+    img
+  )
 }
 
 const writeTexts = texts => img =>
   texts.reduce(writeText, img)
 
 export default ({ events }) => {
-  const mediaCreate = async ({ exchange, high, time, data }) =>
-    registerFont(data.font, 'Custom Font')
-      .then(() => getImageFromFile(data.image))
-      .then(writeTexts(data.texts))
-      .then(imageToBase64)
-      .then(image => events.emit('media.CREATE:DONE', { exchange, high, time, image }))
+  const mediaCreate = ({ exchange, high, time, data }) => pipeAsync(
+    () => registerFont(data.font, 'Custom Font'),
+    () => getImageFromFile(data.image),
+    writeTexts(data.texts),
+    imageToBase64,
+    image => events.emit('media.CREATE:DONE', { exchange, high, time, image }),
+  )()
 
   events.on('media.CREATE', mediaCreate)
 }
