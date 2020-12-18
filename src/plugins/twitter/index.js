13ef280fd91df00b4d5e679d5589f00bdabe5743
Joel Thoms <jthoms@gmail.com>
2020-12-18 11:56:00 -0800

    fix: 🐛 add 'enabled' flag for Twitter for local debugging

--- src/plugins/twitter/index.js
+++ src/plugins/twitter/index.js
@@ -1,15 +1,15 @@
-/* eslint-disable camelcase */
-import Twit from 'twit'
 import config from 'config'
+import fs from 'fs'
 import promisify from 'functional-helpers/promisify'
-import when from 'ramda/src/when'
 import indexOf from 'ramda/src/indexOf'
+import when from 'ramda/src/when'
+import Twit from 'twit'
 
 const hasMimeType = indexOf(',')
 const stripMimeType = image => image.substr(image.indexOf(',') + 1)
 
 export default ({ events }) => {
-  const options = config.get('twitter')
+  const { enabled, ...options } = config.get('twitter')
   const twit = new Twit(options)
   const post = promisify(twit.post, twit)
 
@@ -19,14 +19,16 @@ export default ({ events }) => {
       media_data: newImage
     })
 
-    await post('media/metadata/create', {
-      media_id: media_id_string,
-      alt_text: { text }
-    })
-    await post('statuses/update', {
-      status: text,
-      media_ids: [media_id_string]
-    })
+    const metadata = { media_id: media_id_string, alt_text: { text } }
+    const status = { status: text, media_ids: [media_id_string] }
+
+    if (enabled) {
+      await post('media/metadata/create', metadata)
+      await post('statuses/update', status)
+    } else {
+      const file = Buffer.from(image, 'base64')
+      fs.writeFileSync(process.cwd() + '/DELME.jpg', file)
+    }
 
     events.emit('twitter.POST_MEDIA:DONE', { exchange, high, time, image })
   }
--- src/recipes/all-time-high/events/postMedia.event.js
+++ src/recipes/all-time-high/events/postMedia.event.js
@@ -1,14 +1,9 @@
 import { Observable } from 'rxjs'
-// import fs from 'fs'
 
 export const event = ({ events, log }) =>
   Observable.fromEvent(events, 'media.CREATE:DONE').subscribe(
     ({ exchange, high, time, image, text }) => {
       log.debug('media.CREATE:DONE', exchange, high, time)
-
-      // /* DEBUG: uncomment to debug */
-      // const file = Buffer.from(image, 'base64')
-      // fs.writeFileSync(process.cwd() + '/DELME.jpg', file)
 
       events.emit('twitter.POST_MEDIA', { exchange, high, time, image, text })
     }
