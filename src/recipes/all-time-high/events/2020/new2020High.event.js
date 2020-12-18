90b15b981a539b2d6804b497e636d6c942d3e746
Joel Thoms <jthoms@gmail.com>
2020-12-18 11:58:36 -0800

    fix: 🐛 disable 2020 high

--- src/recipes/all-time-high/events/2020/new2020High.event.js
+++ src/recipes/all-time-high/events/2020/new2020High.event.js
@@ -1,13 +1,15 @@
-import { Observable } from 'rxjs'
+export const event = () => {}
 
-export const event = ({ events, store }) =>
-  Observable.fromEvent(events, 'gdax.TRADE')
-    .filter(store.is2020High)
-    .subscribe(({ exchange, trade }) =>
-      events.emit('core.NEW_2020_HIGH', {
-        exchange,
-        price: trade.price,
-        pair: trade.product_id,
-        time: trade.time
-      })
-    )
+// import { Observable } from 'rxjs'
+
+// export const event = ({ events, store }) =>
+//   Observable.fromEvent(events, 'gdax.TRADE')
+//     .filter(store.is2020High)
+//     .subscribe(({ exchange, trade }) =>
+//       events.emit('core.NEW_2020_HIGH', {
+//         exchange,
+//         price: trade.price,
+//         pair: trade.product_id,
+//         time: trade.time
+//       })
+//     )
